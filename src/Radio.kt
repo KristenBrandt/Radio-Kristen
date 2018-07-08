70a497be1f2582d24dd6b920b90cb1c0207069a0
KristenBrandt <kabrandtf@gmail.com>
2018-07-08 09:53:50 -0600

    Modified src/Radio.kt

---
+++ src/Radio.kt
@@ -0,0 +1,20 @@
+/** Kristen Brandt
+ *  radio ejercicio **/
+class Radio {
+
+    fun main (args: Array<String>) {
+
+
+        val hola = println("")
+        println(hola)
+
+        class radio (
+                /** prender radio **/
+                var encender : boolean = false
+                /** AM o PM**/
+                var AMPM : int
+
+
+        )
+    }
+}

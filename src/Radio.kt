2405799076e8f96003960da8b4a74409eab67df8
KristenBrandt <kabrandtf@gmail.com>
2018-07-08 12:36:16 -0600

    Modification to Radio.kt

--- src/Radio.kt
+++ src/Radio.kt
@@ -1,20 +1,61 @@
 /** Kristen Brandt
  *  radio ejercicio **/
-class Radio {
 
-    fun main (args: Array<String>) {
+/** programa que si sirve **/
+
+fun main (args: Array<String>) {
+
+    var opcionprincipal = 3
+    while (opcionprincipal != 2){
+        println(" 1 : Encender el radio")
+        println(" 2 : Salir del programa")
+        opcionprincipal = readLine()!!.toInt()
+
+        when (opcionprincipal) {
+            1 -> println("El radio se esta encenciendo")
+            2 -> println("Se esta saliendo del programa")
+            else -> println("Ese numero no es valido intente denuevo con un 1 o 2")
+        }
+        }
+}
 
 
-        val hola = println("")
-        println(hola)
+
+
+
+
+
+/** programa que no sirve **/
+
+}
+    var x = 1
+    println(radio_encender $x)
+
+        /** prender radio **/
+        when (x) {
+            1 -> print ("Se esta prendiendo el radio")
+            2 -> print ("Saliendo del programa")
+            else -> throw IllegalArgumentException("Tiene que ser opcion 1 o 2")
+}
+}
+
 
         class radio (
                 /** prender radio **/
+                when (x) {
+                    1 -> print ("Se esta prendiendo el radio")
+                    2 -> print ("Saliendo del programa")
+                    else -> throw IllegalArgumentException ("Tiene que ser opcion 1 o 2")
+            }
+        }
                 var encender : boolean = false
                 /** AM o PM**/
                 var AMPM : int
 
-
-        )
-    }
-}
+                /** volumen **/
+                var max = 100
+                var min = 0
+                if (i in 0..100)
+                    println("El volumen es de : $i ")
+                else println("Porfavor ingrese un numero de 0-100 para el volumen")
+**/
